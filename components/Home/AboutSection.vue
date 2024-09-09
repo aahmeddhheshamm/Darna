e81a68a3a4dad3c3483de92643296bd72ee4bcfd
Dafa <aahmmeddhheshamm@gmail.com>
2024-09-09 01:10:45 -0400

    create new darna project

---
+++ components/Home/AboutSection.vue
@@ -0,0 +1,44 @@
+<script setup>
+import DiscoverImg from "assets/images/discover-img.png";
+import SofaImg from "assets/images/sofa-img.png";
+</script>
+
+<template>
+  <div class="bg-black">
+
+    <div class="grid grid-cols-12">
+      <div class="lg:col-span-7 col-span-12 pt-[63px] mb-72">
+        <h1 class="text-center text-white font-bold md:text-[64px] text-[28px] mb-[9px]">
+          Discover Your New Home
+        </h1>
+
+        <div class="flex items-center justify-between flex-wrap h-full ms-[65px]">
+            <p class="md:text-[24px] text-[14px] font-medium leading-[25px] text-white 2xl:max-w-[485px]  max-w-[385px]">
+              Potter ipsum wand elf parchment wingardium. Nagini quaffle candles hand tears downfall. Fluffy snitch cottage for that sight and. Garg Potter ipsum wand elf parchment wingardium. Nagini quaffle candles hand tears downfall. Fluffy snitch cottage for that sight and. Garg
+            </p>
+        </div>
+      </div>
+      <div class="lg:col-span-5 col-span-12">
+        <div
+            class=" h-[982px] relative"
+            :style="{
+        'background-image': 'url(' + DiscoverImg + ')',
+        'background-size': 'cover',
+        'background-position': 'center',
+        'background-repeat': 'no-repeat'
+      }"
+        >
+          <div class="flex items-center justify-between h-full w-full absolute z-10 my-auto mx-auto 2xl:left-[-500px] lg:left-[-300px] left-0 xl:top-0 top-[-500px]">
+            <div class="2xl:w-[608px] 2xl:h-[543px] md:w-[308px] md:h-[243px] w-[335px] h-[276px]">
+            <img src="@/assets/images/sofa-img.png"  />
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+
+</style>
---
+++ components/Home/TheHeader.vue
@@ -0,0 +1,38 @@
+<script setup>
+import DarnaBg from '@/assets/images/darna-background.png'</script>
+<template>
+
+  <div
+      class="cover-store-profile h-[982px] relative"
+      :style="{
+        'background-image': 'url(' + DarnaBg + ')',
+        'background-size': 'cover',
+        'background-position': 'center',
+        'background-repeat': 'no-repeat'
+      }"
+  >
+    <div class="flex items-center justify-between flex-wrap h-full relative z-10 md:px-[43px] px-[10px] my-auto">
+      <div class="flex flex-col md:gap-[24px] gap-[16px]">
+        <h1 class="md:text-[128px] text-[48px] font-medium leading-[62px] text-white max-w-[596px]">
+        Darna
+        </h1>
+        <p class="md:text-[24px] text-[14px] font-medium leading-[25px] text-white max-w-[666px]">
+          Potter ipsum wand elf parchment wingardium. Nagini quaffle candles hand tears downfall. Fluffy snitch cottage for that sight and. Garg        </p>
+
+      </div>
+    </div>
+
+  </div>
+</template>
+<style scoped>
+
+.cover-store-profile::after {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.90)0%, rgba(0, 0, 0, 0.00)100%);
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+  width: 100%;
+  height: 100%;
+}
+</style>
